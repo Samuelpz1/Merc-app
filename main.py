277837ac704eaac981dd5db9df2031e7104f8ab7
Samuel Padierna <106036575+Samuelpz1@users.noreply.github.com>
2024-11-05 21:59:04 -0500

    research function added , main funtion updated to look for in multiple stores

--- main.py
+++ main.py
@@ -7,41 +7,56 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 import pandas as pd
 import mysql.connector
-num_pages = 10
 
 def main():
-    #Store , link , cards , price , name
+    #Store , link , cards , price , name , search bar , search button 
     info = {
-        'Jumbo': ('https://www.tiendasjumbo.co/supermercado/despensa' , 'div.tiendasjumboqaio-cmedia-integration-cencosud-0-x-galleryItem' , 'div.tiendasjumboqaio-jumbo-minicart-2-x-price','span.vtex-product-summary-2-x-productBrand.vtex-product-summary-2-x-brandName'),
-        'Exito': ('https://www.exito.com/mercado/despensa', 'article.productCard_productCard__M0677','.ProductPrice_container__price__XmMWA','.styles_name__qQJiK'),
-        'D1': ('https://domicilios.tiendasd1.com/ca/alimentos-y-despensa/ALIMENTOS%20Y%20DESPENSA', 'div.styles__StyledCard-sc-3jvmda-0','p.CardBasePrice__CardBasePriceStyles-sc-1dlx87w-0','p.CardName__CardNameStyles-sc-147zxke-0'),
-        'Vaquita': ('https://lavaquita.co/collections/despensa','.productitem','.productitem .price--main .money', '.productitem .productitem--title')
+        'Jumbo': ('https://www.tiendasjumbo.co/supermercado/despensa' , '.tiendasjumboqaio-cmedia-integration-cencosud-0-x-galleryItem' , 'div.tiendasjumboqaio-jumbo-minicart-2-x-price','span.vtex-product-summary-2-x-productBrand.vtex-product-summary-2-x-brandName','.vtex-styleguide-9-x-input','.vtex-store-components-3-x-searchBarIcon'),
+        'Exito': ('https://www.exito.com/mercado/despensa', 'article.productCard_productCard__M0677','.ProductPrice_container__price__XmMWA','.styles_name__qQJiK','[data-testid="store-input"]','[data-testid="store-button"]'),
+        'D1': ('https://domicilios.tiendasd1.com/ca/alimentos-y-despensa/ALIMENTOS%20Y%20DESPENSA', 'div.styles__StyledCard-sc-3jvmda-0','p.CardBasePrice__CardBasePriceStyles-sc-1dlx87w-0','p.CardName__CardNameStyles-sc-147zxke-0','[placeholder="Buscar en Bogota Tienda Domicilios"]','[data-testid="search-action-icon"]'),
+        'Vaquita': ('https://lavaquita.co/collections/despensa','.productitem','.productitem .price--main .money', '.productitem .productitem--title','.form-field-input.live-search-form-field','.live-search-button')
         
     }
-    #research()
-    '''for store in info:
-        research(store ,info[store][0], info[store][1],info[store][2], info[store][3])'''
-    research('Vaquita' ,info['Vaquita'][0], info['Vaquita'][1],info['Vaquita'][2], info['Vaquita'][3])
+    input_u = input('Introduce el nombre del producto que deseas buscar: ')
+    output = []
+    for store in info:
+        store_data = research(store ,info[store][0], info[store][1],info[store][2], info[store][3], info[store][4], info[store][5],input_u)
+        output.append(store_data)
+    #print(research('D1' ,info['D1'][0], info['D1'][1],info['D1'][2], info['D1'][3], info['D1'][4], info['D1'][5],input_u))
 
-def research (store , link , cards , price , name):
+    final_df = pd.concat(output, ignore_index=True)
+    print(f"Estos son los resultados para tu búsqueda en los principales supermercados:\n")
+    print(final_df)
+
+def research (store , link , cards , price , name, search_bar ,search_button, input_u):
+    
     service = Service(ChromeDriverManager().install())
     option = webdriver.ChromeOptions()
-    #option.add_argument("--headless")
+    option.add_argument("--headless")
 
     #Open the website
     option.add_argument("--window-size=1920,1800")
     driver = Chrome(service=service,options=option)
     driver.get(link)
-    time.sleep(20)
+    time.sleep(2)
 
     #Creting empty df
     products_data = []
     product_names_seen = set()  # Set to keep track of unique product names
 
+    #Search the product
+    driver.find_element(By.CSS_SELECTOR,search_bar).send_keys(input_u)
+    time.sleep(2)
+    driver.find_element(By.CSS_SELECTOR,search_button).click()
+    time.sleep(2)
+
+
 
     #Getting prices
 
     last_height = driver.execute_script("return document.body.scrollHeight")
+    products_collected = 0  # Variable para contar los productos recopilados
+    max_products = 3  # Número máximo de productos a recopilar
     while True:
         # Desplazarse hacia abajo en pequeños intervalos
         driver.execute_script("window.scrollBy(0, 500);")
@@ -56,13 +71,21 @@ def research (store , link , cards , price , name):
             name_element = name_element.text
             price_element = price_element.text
             price_element = float(price_element.replace('$', '').replace('.', '').replace(',', '.'))
-            print(name_element,price_element)
+            #Print(name_element,price_element)
 
 
             # Verificar si ya hemos visto este producto
             if name_element not in product_names_seen:
                 products_data.append({"Product Name": name_element, "Price": price_element, "Store": store})
                 product_names_seen.add(name_element)  # Añadir el nombre del producto al set
+                products_collected += 1  # Incrementar el contador de productos
+
+            # Si ya se han recolectado los tres productos, salir del bucle
+            if products_collected >= max_products:
+                break
+        # Si hemos recolectado los tres productos, salimos del bucle
+        if products_collected >= max_products:
+            break
 
         # Scroll incremental hacia abajo
         driver.execute_script("window.scrollBy(0, 500);")
@@ -78,6 +101,8 @@ def research (store , link , cards , price , name):
 
     df = pd.DataFrame(products_data)
     
+    return df 
+'''
     db_connection = mysql.connector.connect(
         host = 'localhost',
         user = 'root',
@@ -98,7 +123,7 @@ def research (store , link , cards , price , name):
 
     # Cerrar la conexión
     cursor.close()
-    db_connection.close()
+    db_connection.close()'''
 
 if __name__ == "__main__":
     main()
