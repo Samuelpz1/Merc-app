c3178b688cb96218ca6caf32cba4890500ba16a9
samuelpz1 <samuelpz9912@gmail.com>
2024-11-07 23:56:14 -0500

    Try and except added inside while cicle

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ def main():
     info = {
         'Jumbo': ('https://www.tiendasjumbo.co/supermercado/despensa' , '.tiendasjumboqaio-cmedia-integration-cencosud-0-x-galleryItem' , 'div.tiendasjumboqaio-jumbo-minicart-2-x-price','span.vtex-product-summary-2-x-productBrand.vtex-product-summary-2-x-brandName','.vtex-styleguide-9-x-input','.vtex-store-components-3-x-searchBarIcon'),
         'Exito': ('https://www.exito.com/mercado/despensa', 'article.productCard_productCard__M0677','.ProductPrice_container__price__XmMWA','.styles_name__qQJiK','[data-testid="store-input"]','[data-testid="store-button"]'),
-        'D1': ('https://domicilios.tiendasd1.com/ca/alimentos-y-despensa/ALIMENTOS%20Y%20DESPENSA', 'div.styles__StyledCard-sc-3jvmda-0','p.CardBasePrice1__CardBasePriceStyles-sc-1dlx87w-0','p.CardName1__CardNameStyles-sc-147zxke-0','[placeholder="Buscar en Bogota Tienda Domicilios"]','[data-testid="search-action-icon"]'),
+        'D1': ('https://domicilios.tiendasd1.com/ca/alimentos-y-despensa/ALIMENTOS%20Y%20DESPENSA', 'div.styles__StyledCard-sc-3jvmda-0','p.CardBasePrice__CardBasePriceStyles-sc-1dlx87w-0','p.CardName__CardNameStyles-sc-147zxke-0','[placeholder="Buscar en Bogota Tienda Domicilios"]','[data-testid="search-action-icon"]'),
         'Vaquita': ('https://lavaquita.co/collections/despensa','.productitem','.productitem .price--main .money', '.productitem .productitem--title','.form-field-input.live-search-form-field','.live-search-button')
         
     }
